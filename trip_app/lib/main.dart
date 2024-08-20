831918c8344898ec5e085797cb4ad9454ba97810
Rewat Boonsit <ravatna@gmail.com>
2024-08-20 18:13:36 +0700

    [core] updte main

--- trip_app/lib/main.dart
+++ trip_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'login_page.dart';
-import 'tab_base_page.dart';
+import 'pages/login_page.dart';
+import 'pages/tab_base_page.dart';
 
 void main() {
   runApp(MyApp());
